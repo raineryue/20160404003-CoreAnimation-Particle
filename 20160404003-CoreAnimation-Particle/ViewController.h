fa222d19bf67914615f68712264e4f272c9291e6
raineryue <it_jinlei.yue@163.com>
2016-04-05 22:22:26 +0800

    Initial Commit

---
+++ 20160404003-CoreAnimation-Particle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160404003-CoreAnimation-Particle
+//
+//  Created by Rainer on 16/4/5.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 20160404003-CoreAnimation-ParticleTests/_0160404003_CoreAnimation_ParticleTests.m
@@ -0,0 +1,39 @@
+//
+//  _0160404003_CoreAnimation_ParticleTests.m
+//  20160404003-CoreAnimation-ParticleTests
+//
+//  Created by Rainer on 16/4/5.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _0160404003_CoreAnimation_ParticleTests : XCTestCase
+
+@end
+
+@implementation _0160404003_CoreAnimation_ParticleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
